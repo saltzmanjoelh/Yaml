98226435ff4bcbbb50a63cd33aec64949ad8fd54
Joel Saltzman <talk@joelsaltzman.com>
2018-03-26 00:17:42 -0700

    Added static lib type

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Yaml",
+            type: .static,
             targets: ["Yaml"]),
     ],
     dependencies: [
